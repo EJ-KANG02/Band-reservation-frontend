26b1ab46c386176d1008672931cfe3a087645d68
eunji <enji4@naver.com>
2026-04-19 15:51:55 +0900

    chore: bump APP_VERSION to 1.5

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-const APP_VERSION = '1.4'
+const APP_VERSION = '1.5'
 
 const storedVersion = localStorage.getItem('appVersion')
 if (storedVersion !== APP_VERSION) {
